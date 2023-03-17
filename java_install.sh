c7644c26b049051542550dc053154c83b8a047a3
suyash-srivastava-dev <Git@New@1>
2023-03-17 17:12:06 +0530

    yes flag

--- java_install.sh
+++ java_install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #install java env
 sudo apt update
-sudo apt install default-jre
+sudo apt install default-jre -y
 java -version
-sudo apt install default-jdk
+sudo apt install default-jdk -y
 javac -version
 #install maven
-sudo apt install maven
+sudo apt install maven -y
 
 # #install kafka and unzip the folder
 # wget https://mirrors.estointernet.in/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
