f5b1f8ae357489e0a26e93446fcfb34ee926e62b
suyash-srivastava-dev <Git@New@1>
2023-03-17 17:23:40 +0530

    kafka installation steps

--- kafka_install.sh
+++ kafka_install.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-
+echo "Downloading kafka"
 wget https://dlcdn.apache.org/kafka/3.4.0/kafka_2.13-3.4.0.tgz
+echo "Unzip kafka"
 tar -xzf kafka_2.13-3.4.0.tgz
+echo "change dir kafka"
 cd kafka_2.13-3.4.0
+echo "Start the ZooKeeper service : bin/zookeeper-server-start.sh config/zookeeper.properties"
 
-# Start the ZooKeeper service
-# bin/zookeeper-server-start.sh config/zookeeper.properties
-
-# Start the Kafka broker service
-# bin/kafka-server-start.sh config/server.properties
+echo "Start the Kafka broker service : bin/kafka-server-start.sh config/server.properties"
